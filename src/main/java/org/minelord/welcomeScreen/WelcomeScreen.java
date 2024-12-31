b3beac8a4edbd793d8ae169c1e78b5fa8f514fce
agarciacorte <alex.garcia.corte@gmail.com>
2024-12-30 22:38:59 -0600

    refactored code

--- src/main/java/org/minelord/welcomeScreen/WelcomeScreen.java
+++ src/main/java/org/minelord/welcomeScreen/WelcomeScreen.java
@@ -20,15 +20,12 @@ public final class WelcomeScreen extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Cargar el archivo de configuración
-        saveDefaultConfig(); // Si no existe el archivo, lo crea con el archivo predeterminado
+        saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
-    public void onDisable() {
-        // Lógica de apagado del plugin
-    }
+    public void onDisable() {}
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -38,29 +35,23 @@ public final class WelcomeScreen extends JavaPlugin implements Listener {
 
         Bukkit.getRegionScheduler().runDelayed(this, player.getLocation(), (task) -> {
 
-            // Convertir el displayName del jugador a texto plano
             String playerName = miniMessage.serialize(player.displayName());
             String serverName = config.getString("server_name");
 
-            // Obtener el idioma preferido del jugador (por ejemplo, en español 'es')
-            String language = player.getLocale().split("_")[0]; // Obtiene el idioma preferido
+            String language = player.getLocale().split("_")[0];
 
-            // Obtener el título y subtítulo correspondientes desde el archivo de configuración
 
             String titleMessage = config.getString("messages." + language + ".title");
             String subtitleMessage = config.getString("messages." + language + ".subtitle");
 
-            // Si no se encuentra el idioma, se usa el idioma predeterminado (inglés)
             if (titleMessage == null || subtitleMessage == null) {
                 titleMessage = config.getString("messages.en.title");
                 subtitleMessage = config.getString("messages.en.subtitle");
             }
 
-            // Reemplazar %player% y %servername% con los valores correspondientes
             titleMessage = titleMessage.replace("%player%", playerName).replace("%servername%", serverName);
             subtitleMessage = subtitleMessage.replace("%player%", playerName).replace("%servername%", serverName);
 
-            // Crear el título y subtítulo usando Adventure
             Component title = miniMessage.deserialize(titleMessage);
             Component subtitle = miniMessage.deserialize(subtitleMessage);
 
@@ -70,7 +61,6 @@ public final class WelcomeScreen extends JavaPlugin implements Listener {
                     Title.Times.times(Duration.ofSeconds(1), Duration.ofSeconds(5), Duration.ofSeconds(1))
             );
 
-            // Mostrar el título al jugador
             player.showTitle(welcomeTitle);
 
         }, 20L);
